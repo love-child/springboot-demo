f0ca9cd10b0f8c666e4e0a5f53f9a1133caba673
SunBo1994 <sunboEIP@163.com>
2020-08-06 14:35:11 +0800

    初始化 springboot项目

---
+++ src/main/java/com/lovebaby/LoveBabyApplication.java
@@ -0,0 +1,15 @@
+package com.lovebaby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * author 孙博
+ * date 2020/8/6 14:30
+ */
+@SpringBootApplication
+public class LoveBabyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LoveBabyApplication.class, args);
+    }
+}
---
+++ src/test/java/LoveBabyApplicationTests.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * author 孙博
+ * date 2020/8/6 14:31
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoveBabyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
